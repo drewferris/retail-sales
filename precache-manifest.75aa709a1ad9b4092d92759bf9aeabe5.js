67564802852d596f652cb7decd1361d2f63447fd
Drew Ferris <v-drferr@microsoft.com>
2020-07-28 20:54:17 -0700

    Updates

---
+++ precache-manifest.75aa709a1ad9b4092d92759bf9aeabe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8701e167785fb602718f5557af4e0de1",
+    "url": "/drewferris/retail-sales/index.html"
+  },
+  {
+    "revision": "900a688e23aa0a95789a",
+    "url": "/drewferris/retail-sales/static/css/main.e0081b70.chunk.css"
+  },
+  {
+    "revision": "8c437b623fc1dabd9202",
+    "url": "/drewferris/retail-sales/static/js/2.c344f2c5.chunk.js"
+  },
+  {
+    "revision": "59a0a07934ba5e7c0fd5281c0f1901d4",
+    "url": "/drewferris/retail-sales/static/js/2.c344f2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900a688e23aa0a95789a",
+    "url": "/drewferris/retail-sales/static/js/main.90b9bda3.chunk.js"
+  },
+  {
+    "revision": "a6c64400269d7d8c710c",
+    "url": "/drewferris/retail-sales/static/js/runtime-main.e74a761e.js"
+  }
+]);
